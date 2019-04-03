365834fbd75138bc492dcdc3ab6771886a1904a6
Marshall Offutt <marshalloffutt@gmail.com>
2019-04-02 20:17:20 -0500

    successful test 01, 1 converts to i

---
+++ BadSuperbowlNamer.Tests/converting_numbers_to_roman_numerals.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace BadSuperbowlNamer.Tests
+{
+    public class converting_numbers_to_roman_numerals
+    {
+        [Fact]
+        public void converting_1_should_be_I()
+        {
+            //Arrange -- context             -- Given
+            var converter = new RomanNumeralizer();
+            var input = 1;
+            var expectedResult = "I";
+
+            //Act     -- do the thing        -- When
+            var actualResult = converter.ConvertMyShit(input);
+
+            //Assert  -- checking the result -- Then
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ BadSuperbowlNamer/RomanNumeralizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadSuperbowlNamer
+{
+    public class RomanNumeralizer
+    {
+        public string ConvertMyShit(int numberToConvert)
+        {
+            return "I";
+        }
+    }
+}
