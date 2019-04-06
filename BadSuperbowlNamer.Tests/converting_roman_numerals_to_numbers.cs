012313952487280a493fb34be7bde8770fc413bd
Marshall Offutt <marshalloffutt@gmail.com>
2019-04-06 11:03:05 -0500

    I converts to 1

---
+++ BadSuperbowlNamer.Tests/converting_roman_numerals_to_numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace BadSuperbowlNamer.Tests
+{
+    public class converting_roman_numerals_to_numbers
+    {
+        [Fact]
+        public void test01_converting_I_should_be_1()
+        {
+            var converter = new Numberizer();
+            var input = "I";
+            int expectedResult = 1;
+
+            var actualResult = converter.ConvertToNumber(input);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ BadSuperbowlNamer/RomanToInteger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadSuperbowlNamer
+{
+    public class Numberizer
+    {
+        public int ConvertToNumber(string romanNumeral)
+        {
+            var number = 0;
+            if (romanNumeral == "I")
+            {
+                number = 1;
+            }
+            return number;
+
+        }
+    }
+}
